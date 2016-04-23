4546efe0e8767a3e247416b77f8304467cb768a8
[A <scullingnull+lisp@gmail.com>
2016-04-23 14:03:11 -0700

    Scopes sucesfully passed

---
+++ scopes.js
@@ -0,0 +1,10 @@
+function foo() {
+  var bar;
+}
+// ## Notes
+//
+//   * [1]: There are also 4 other scopes in the language: Global, `with`, `catch`,
+//   *      and `eval`. These tend not to be used much, so we will ignore them.
+//   *
+//   *
+//   * [2]: This workshop will concentrate only on Lexical Scoping.
