ffb00a43cb9a2f66a84b600baeef38594cd35707
lispincsclothing <scullingnull+lisp@gmail.com>
2016-05-21 17:03:54 -0700

    Http server

--- stream-adventure.js
+++ stream-adventure.js
@@ -1,35 +1,38 @@
+var http = require('http');
+var fs = require('fs');
+var through = require('through2');
+
+function write(buf, _, next) {
+  this.push(buf.toString().toUpperCase());
+  next();
+}
+
+function end(done) {
+  done();
+}
+
+var server = http.createServer(function(req, res) {
+  if (req.method === 'POST') {
+    // req.pipe(fs.createWriteStream('post.txt'));
+    // req.pipe(through(write, end)).pipe(fs.createWriteStream('post.txt'));
+    req.pipe(through(write, end)).pipe(res);
+  }
+  // res.end('beep boop\n');
+});
+server.listen(process.argv[2]);
+
 // Here's the reference solution:
-
-var concat = require('concat-stream');
-
-process.stdin.pipe(concat(function(src) {
-    var s = src.toString().split('').reverse().join('');
-    console.log(s);
-}));
-
-// You will be given text on process.stdin. Buffer the text and reverse it using
-// the `concat-stream` module before writing it to stdout.
-//
-// `concat-stream` is a write stream that you can pass a callback to get the
-// complete contents of a stream as a single buffer. Here's an example that uses
-// concat to buffer POST content in order to JSON.parse() the submitted data:
-//
-//     var concat = require('concat-stream');
-//     var http = require('http');
-//
-//     var server = http.createServer(function (req, res) {
-//         if (req.method === 'POST') {
-//             req.pipe(concat(function (body) {
-//                 var obj = JSON.parse(body);
-//                 res.end(Object.keys(obj).join('\n'));
-//             }));
-//         }
-//         else res.end();
-//     });
-//     server.listen(5000);
-//
-// In your adventure you'll only need to buffer input with `concat()` from
-// process.stdin.
-//
-// Make sure to `npm install concat-stream` in the directory where your solution
-// file is located.
+  // 
+  // var http = require('http');
+  // var through = require('through2');
+  //
+  // var server = http.createServer(function (req, res) {
+  //     if (req.method === 'POST') {
+  //         req.pipe(through(function (buf, _, next) {
+  //             this.push(buf.toString().toUpperCase());
+  //             next();
+  //         })).pipe(res);
+  //     }
+  //     else res.end('send me a POST\n');
+  // });
+  // server.listen(parseInt(process.argv[2]));
